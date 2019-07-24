1123718dafa3c40330f1cb33de11a528a625b247
aeseedhouse <37798059+aeseedhouse@users.noreply.github.com>
2019-07-24 11:59:11 +1000

    Revert "Added adaptive time step method and bug fixes"

--- arnoldi.m
+++ arnoldi.m
@@ -19,28 +19,28 @@ function [gamma,vKrylov,w] = arnoldi(A,v,L)
     % w = v in krylov basis
     w = zeros(D,1);
     w(1) = sqrt(v'*v); 
-    if any(v) % if v is zero vector, return gamma, vKrylov and w as all zeros
-        vKrylov(:,1) = v/w(1); % first basis vector is normalised input vector
+    
+    vKrylov(:,1) = v/w(1); % first basis vector is normalised input vector
+    
+    for jj = 1:L
+        v_j1 = A*vKrylov(:,jj);
         
-        for jj = 1:L
-            v_j1 = A*vKrylov(:,jj);
-            
-            % calculate projections of v_j1 onto krylov basis vectors and
-            % remove from v_j1
-            for ii=0:jj-1
-                gamma(ii+1,jj) = vKrylov(:,ii+1)'*v_j1;
-                v_j1 = v_j1 - gamma(ii+1,jj)*vKrylov(:,ii+1);
-            end
-            
-            if jj < L   % don't save last vector and extra row of gamma
-                if abs(v_j1) < 1e-6    % dimension of krylov space larger than dimension of v - gram schmidt starts producing zero vectors
-                    gamma(jj+1,jj) = 0;
-                    vKrylov(:,jj+1) = 0;
-                else
-                    gamma(jj+1,jj) = sqrt(v_j1'*v_j1);    % norm of v_j1
-                    vKrylov(:,jj+1) = v_j1/gamma(jj+1,jj);    % normalise v_j1
-                end
+        % calculate projections of v_j1 onto krylov basis vectors and
+        % remove from v_j1
+        for ii=0:jj-1            
+            gamma(ii+1,jj) = vKrylov(:,ii+1)'*v_j1;
+            v_j1 = v_j1 - gamma(ii+1,jj)*vKrylov(:,ii+1);
+        end
+        
+        if jj < L   % don't save last vector and extra row of gamma
+            if abs(v_j1) < 1e-6    % dimension of krylov space larger than dimension of v - gram schmidt starts producing zero vectors
+                gamma(jj+1,jj) = 0;
+                vKrylov(:,jj+1) = 0;
+            else
+                gamma(jj+1,jj) = sqrt(v_j1'*v_j1);    % norm of v_j1
+                vKrylov(:,jj+1) = v_j1/gamma(jj+1,jj);    % normalise v_j1
             end
         end
     end
+
 end
