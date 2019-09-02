ded99f00bfc3d7e21ca37739760e44f0420f2f28
aeseedhouse <aeseedhouse@gmail.com>
2019-09-02 16:21:19 +1000

    Added adaptive steps

--- testRabi.m
+++ testRabi.m
@@ -4,9 +4,9 @@ clear;clc;close all;
 % parameters
 psi0 = [1;0]; % initial state
 D = length(psi0);
-M = 7;  % number of points to sample in polynomial series approximation of s_ext
+M = 10;  % number of points to sample in polynomial series approximation of s_ext
 L = M;    % number of points to sample in polynomial approximation f(G,t)
-fRabi = 1e6;    % rabi frequency
+fRabi = 1.2e6;    % rabi frequency
 threshold = 1e-6;   % threshold for convergence checks
 
 t = linspace(0,1,100)*1.5e-6;     % desired times to calculate psi(t)
@@ -14,7 +14,7 @@ tStep = 1e-6;   % length of domain for approximation of s_ext(t)
 calcH = @rabiHam;   % handle for function which returns the hamiltonian at given times
 
 %% calculate psi(t)
-psi = evolve(t,tStep,M,L,psi0,calcH,threshold,fRabi);
+psi = evolveAdaptive(t,M,L,psi0,calcH,threshold,[],[],fRabi);
 
 %% Plot
 pX = zeros(size(t));
@@ -26,6 +26,7 @@ for ii=1:length(t)
     pY(ii) = abs([1/sqrt(2),-1i/sqrt(2)]*psi(:,ii))^2;
 end
 
+figure()
 plot(t,pX)
 hold on
 plot(t,pY)
